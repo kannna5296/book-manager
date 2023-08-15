346401c4669b4f8cdafec53f5ff3c84cba58fdba
kannna5296 <kannnahacks5296@gmail.com>
2023-08-15 23:23:05 +0900

    API呼び出し

--- src/components/Learn.vue
+++ src/components/Learn.vue
@@ -1,7 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { LearnedRepository } from "../repositories/generated";
+const responses =
+  //TODO TakeLatest
+  await LearnedRepository.index().then((res) => {
+    return res.learnedList;
+  });
+// .catch((error) => {
+//   console.log("しっぱい!" + error);
+//   return [];
+// });
+</script>
 
 <template>
   <h2>今までの学び一覧</h2>
+  <li v-for="res in responses">
+    {{ res.content }}
+  </li>
   <v-row align-content="center" class="mt-10 w-50 mx-auto"> 8/1 </v-row>
   <v-row align-content="center" class="mt-10 w-50 mx-auto"> 8/2 </v-row>
   <v-row align-content="center" class="mt-10 w-50 mx-auto"> 8/3 </v-row>
