623e4cc5eb9a3fdd25bee49f6564a626ae10f444
kannna5296 <kannnahacks5296@gmail.com>
2023-09-10 12:46:08 +0900

    投稿ページ作成(送信部分はまだ)

--- src/components/LearnAdd.vue
+++ src/components/LearnAdd.vue
@@ -1,5 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+const learnRequiredValidation = (value: string) =>
+  !!value || "今日の学びを入力してください。";
+const learnMaxValidation = (value: string) =>
+  value.length <= 140 || "140文字以内で入力してください。";
+const learnRules = [learnRequiredValidation, learnMaxValidation];
+
+const valid = ref(false);
+</script>
 
 <template>
   <h2 class="mt-10 w-50 mx-auto">今日の学び,気づきは？</h2>
+  <v-form ref="form" v-model="valid">
+    <v-textarea
+      class="mt-10 w-50 mx-auto"
+      label="Label"
+      variant="outlined"
+      :rules="learnRules"
+    ></v-textarea>
+    <div class="mt-10 w-50 mx-auto">
+      <v-btn :disabled="!valid" color="indigo-darken-3">投稿</v-btn>
+    </div>
+  </v-form>
 </template>
