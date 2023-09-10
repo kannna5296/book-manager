c070756e992482ae093dc82b476d0a7e850effed
kannna5296 <kannnahacks5296@gmail.com>
2023-09-10 11:54:55 +0900

    mock増やし

--- src/mock/learn/index.ts
+++ src/mock/learn/index.ts
@@ -5,8 +5,25 @@ import { LearnedIndexResponse } from "../../repositories/generated";
 const responseList: LearnedIndexResponse[] = [
   {
     id: "01H75MAQSCQCBJ5MQJHYWZ5KHT",
-    date: "2023-01-01",
+    date: "2023-01-06",
     content: "最悪売れなくてもいいから個人開発はなんかまず一個作れ",
+  },
+  {
+    id: "01H75MAQSCQCBJ5MQJHYWZ5KHR",
+    date: "2023-01-05",
+    content:
+      "エンジニアチームマネジメントするにも設計力、引き出しの多さが必要(まだ仮説)",
+  },
+  {
+    id: "01H75MAQSCQCBJ5MQJHYWZ5KHR",
+    date: "2023-01-04",
+    content:
+      "疲れた時はNoイヤホンで散歩するに限る。脳が休まるのとアイデアが出てくる",
+  },
+  {
+    id: "01H75MAQSCQCBJ5MQJHYWZ5KHR",
+    date: "2023-01-03",
+    content: "「いかにしなくて良い作業を決めるか」も仕事のうち",
   },
   {
     id: "01H75MAQSCQCBJ5MQJHYWZ5KHA",
@@ -15,7 +32,7 @@ const responseList: LearnedIndexResponse[] = [
   },
   {
     id: "01H75MAQSCQCBJ5MQJHYWZ5KHR",
-    date: "2023-01-02",
+    date: "2023-01-01",
     content: "小説書きとランニングは似ている",
   },
 ];
