5265d8ff8f56ebaa8cee421bbb7e4c25ce162812
kannna5296 <kannnahacks5296@gmail.com>
2023-09-16 20:17:44 +0900

    CORS回避のためのProxyせってい

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, ProxyOptions, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  const { VITE_API_BASE_PATH } = env;
+
+  const proxy: Record<string, ProxyOptions | string> = {
+    //これほんまにいける？
+    [`^${VITE_API_BASE_PATH}`]: {
+      target: VITE_API_BASE_PATH,
+      changeOrigin: true,
+    },
+  };
   return {
     plugins: [vue()],
   };
