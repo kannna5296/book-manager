1aca87fb4fb9999afec8cb77bc0fe69a0d4fe2e9
kannna5296 <kannnahacks5296@gmail.com>
2023-09-23 14:48:17 +0900

    user取得をメソッドわけ

--- src/components/LearnAdd.vue
+++ src/components/LearnAdd.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { collection, addDoc, Timestamp } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  Timestamp,
+  query,
+  where,
+} from "firebase/firestore";
 import { db } from "../firebase/firebase.ts";
 import { auth } from "../firebase/firebase.ts";
 import { useRouter } from "vue-router";
@@ -14,45 +20,41 @@ const learnRules = [learnRequiredValidation, learnMaxValidation];
 const valid = ref(false);
 const inputting = ref("");
 const router = useRouter();
+const userId = ref("");
+
+auth.onAuthStateChanged((user) => {
+  if (!user) {
+    console.log("user is null");
+    return;
+  }
+  userId.value = user.uid;
+});
 
 const submit = async () => {
   console.log("submitting...");
+  console.log(userId.value);
 
-  auth.onAuthStateChanged((user) => {
-    if (!user) {
-      console.log("user is null");
-      return;
-    }
-    addDoc(collection(db, "learned"), {
-      content: inputting.value,
-      userId: user?.uid,
-      createdAt: Timestamp.now(),
-      updatedAt: Timestamp.now(),
+  const hoge = query(
+    collection(db, "learned"),
+    where("userId", "==", userId.value)
+  );
+  if (hoge != null) return;
+
+  addDoc(collection(db, "learned"), {
+    content: inputting.value,
+    userId: userId.value,
+    createdAt: Timestamp.now(),
+    updatedAt: Timestamp.now(),
+  })
+    .then((result) => {
+      console.log("submit success!");
+      console.log(result);
+      router.push("/");
     })
-      .then((result) => {
-        console.log("submit success!");
-        console.log(result);
-        router.push("/");
-      })
-      .catch((error) => {
-        console.log("submit failed!!");
-        console.log(error);
-      });
-  });
-  //TODO TakeLatest
-  //TODO catchとかは共通化したい
-  // const params: LearnedRegisterForm = {
-  //   content: inputting.value,
-  // };
-  // await LearnedRepository.register({ requestBody: params })
-  //   .then(() => {
-  //     console.log("OK!");
-  //     router.replace("/");
-  //     //TODO notify
-  //   })
-  //   .catch((error) => {
-  //     console.log("Error!" + error);
-  //   });
+    .catch((error) => {
+      console.log("submit failed!!");
+      console.log(error);
+    });
 };
 </script>
 
