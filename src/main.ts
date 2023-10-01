40574be8dff4eb743c20e245f2ea2decfb0c875f
kannna5296 <kannnahacks5296@gmail.com>
2023-10-01 16:58:31 +0900

    favicon

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,8 @@ import App from "./App.vue";
 import { worker } from "./mock/worker";
 import routes from "./router/router";
 import { vuetify } from "./plugins/vuetify";
-// // Vuetify
-// import "vuetify/styles";
-// import { createVuetify } from "vuetify";
-// import * as components from "vuetify/components";
-// import * as directives from "vuetify/directives";
-
-// const vuetify = createVuetify({
-//   components,
-//   directives,
-// });
 
 if (import.meta.env.MODE === "mock") {
-  console.log("あああ");
   worker.start({
     serviceWorker: {
       url: "/mockServiceWorker.js",
