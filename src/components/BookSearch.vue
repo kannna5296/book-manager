ea7e6989f92f30b6377fc3979f8e1199adcdafae
yamane <kannnahacks5296@gmail.com>
2024-02-10 20:42:47 +0900

    url更新を発火点とした検索条件Fetch追加

--- src/components/BookSearch.vue
+++ src/components/BookSearch.vue
@@ -1,25 +1,74 @@
 <script setup lang="ts">
 import { BookRepository, PageBookSearchResponse } from '@/repositories/generated';
 import { ref } from 'vue'
+import { queryToString } from '@/util/util'
+import {
+  LocationQuery,
+  onBeforeRouteUpdate,
+  useRoute,
+  useRouter,
+} from "vue-router";
+
+const router = useRouter();
+const route = useRoute();
 
 const searchResult = ref<PageBookSearchResponse>()
 
+// TODO 一つの検索条件オブジェクトにしたい
+const id = ref<string>();
+const name = ref<string>();
+const author = ref<string>();
+
+const updateDataFromUrlQuery = (query: LocationQuery): void => {
+  id.value = queryToString(query.id)
+  name.value = queryToString(query.name);
+  author.value = queryToString(query.author)
+};
+
 const createSearchParams = () => ({
-  // TODO URLから取得する
-  id: "a",
-  name: "aa",
-  author: "hoge",
+  id: id.value,
+  name: name.value,
+  author: author.value,
   page: 1,
   size: 10
 })
+
 const getBooks = () => {
   const searchParams = createSearchParams();
-  BookRepository.search(searchParams).then((result) => {
-    // useTakeLatest的なWrap関数あ作る
-    searchResult.value = result
-  }
-  )
+  BookRepository.search(searchParams)
+    .then((result) => {
+      // useTakeLatest的なWrap関数あ作る
+      searchResult.value = result
+    }
+    )
 }
+
+const updateUrlQueryFromData = (): void => {
+  router.push({
+    query: {
+      id: id.value || undefined,
+      name: name.value || undefined,
+      author: author.value || undefined,
+    },
+  });
+};
+
+const onSearch = () => {
+  // ①Urlを変更することで検索発火
+  updateUrlQueryFromData();
+}
+
+onBeforeRouteUpdate(async (to, _, next) => {
+  // ②Urlを元にv-model変更
+  updateDataFromUrlQuery(to.query);
+  next();
+  // ③v-modelの値でAPI叩く
+  getBooks();
+});
+
+
+// ロードした時にも検索条件入っていて欲しい
+updateDataFromUrlQuery(route.query);
 getBooks();
 </script>
 
@@ -29,16 +78,18 @@ getBooks();
     <!-- Component化 -->
     <div class="mb-1 align-center">
       ID
-      <v-text-field density="compact" variant="outlined" placeholder="1"></v-text-field>
+      <v-text-field density="compact" variant="outlined" placeholder="1" v-model="id"></v-text-field>
     </div>
     <div class="mb-1 align-center">
       書籍名
-      <v-text-field density="compact" variant="outlined" placeholder="人を動かす"></v-text-field>
+      <v-text-field density="compact" variant="outlined" placeholder="人を動かす" v-model="name"></v-text-field>
     </div>
     <div class="mb-1 align-center">
       著者名
-      <v-text-field density="compact" variant="outlined" placeholder="D・カーネギー"></v-text-field>
+      <v-text-field density="compact" variant="outlined" placeholder="D・カーネギー" v-model="author"></v-text-field>
     </div>
+
+    <v-btn @click="onSearch">検索</v-btn>
     <v-table theme="light">
       <thead>
         <tr>
